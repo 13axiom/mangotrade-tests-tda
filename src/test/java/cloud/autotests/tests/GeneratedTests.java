51765c7f1beee6a377980f0c9f6e302a3f39eb7a
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-03-28 20:18:26 +0300

    commit6

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
@@ -16,8 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GeneratedTests extends TestBase {
     @Test
-    @Description("Soon to be implemented by you (or QA.GURU engineers)")
-    @DisplayName("Login to account")
+    @Description("Login test 1")
+    @DisplayName("Success login to account")
     void generatedTest() {
         step("Open 'https://trade.mangotrade.com/en/login'", () -> {
             step("// todo: just add selenium action", () -> {
@@ -50,6 +51,29 @@ public class GeneratedTests extends TestBase {
             step("// todo: just add selenium action", () -> {
                 String currURL = getWebDriver().getCurrentUrl();
                 assertEquals("https://trade.mangotrade.com/traderoom", currURL);
+            });
+        });
+    }
+
+    @Test
+    @Description("Login test 2")
+    @DisplayName("Failed login with null data")
+    void generatedTest() {
+        step("Open 'https://trade.mangotrade.com/en/login'", () -> {
+                open("https://trade.mangotrade.com/en/login");
+        });
+
+
+        step("Click button \"Log in\"", () -> {
+                $("[data-test-id=login-submit-button]").click();
+        });
+
+        step("Expected Results", () -> {
+            step("Error message below field 'Email'", () -> {
+                $("[name=identifier]").$(".iqInput__error.active").shouldHave(text("Fill out the field"));
+            });
+            step("Error message below field 'Password'", () -> {
+                $("[name=password]").$(".iqInput__error.active").shouldHave(text("Fill out the field"));
             });
         });
     }
