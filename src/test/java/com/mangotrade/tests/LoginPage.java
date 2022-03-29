9ae7a32a654c812d9a37c6ce3b1ee272d35d1d9d
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-03-29 12:42:33 +0300

    design v.1.3

--- src/test/java/com/mangotrade/tests/LoginPage.java
+++ src/test/java/com/mangotrade/tests/LoginPage.java
@@ -174,16 +174,15 @@ public class LoginPage extends TestBase {
         });
 
         step("Check redirect to registration form", () -> {
-            step("Check form header -> should be 'Sing Up'", () -> {
-                $(".RegisterFormContainer").$("h1").shouldHave(exactText("Sign Up"));
-            });
-
             step("Check link", () -> {
-                sleep(5000);
+                sleep(10000);
                 String currURL = getWebDriver().getCurrentUrl();
             assertEquals("https://trade.mangotrade.com/en/register", currURL);
             });
 
+            step("Check form header -> should be 'Sing Up'", () -> {
+                $(".RegisterFormContainer").$("h1").shouldHave(exactText("Sign Up"));
+            });
         });
     }
 }
