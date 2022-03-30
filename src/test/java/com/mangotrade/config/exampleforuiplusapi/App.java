cf706f3f6fc8ff5067d6dcd867b1ce61544a6b4d
DmitriyTQC <84909251+DmitriyTQC@users.noreply.github.com>
2022-03-30 12:49:29 +0300

    Delete src/test/java/com/mangotrade/config/exampleforuiplusapi directory

--- src/test/java/com/mangotrade/config/exampleforuiplusapi/App.java
+++
@@ -1,7 +0,0 @@
-package com.mangotrade.config.exampleforuiplusapi;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class App {
-    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
-}
