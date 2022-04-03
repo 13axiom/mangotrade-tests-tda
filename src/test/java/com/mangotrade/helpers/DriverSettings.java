510c6f8ea707d3c844346f8033bc6c6837d2042e
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-04-03 12:15:54 +0300

    set/getProperty and issue

--- src/test/java/com/mangotrade/helpers/DriverSettings.java
+++ src/test/java/com/mangotrade/helpers/DriverSettings.java
@@ -11,7 +11,8 @@ import java.util.Map;
 public class DriverSettings {
 
     public static void configure() {
-        System.setProperty("url","mango");//todo работает отсюда, но из консоли не позволяет менять проперти файл, че за нах?
+       // System.setProperty("url","mango");//todo работает отсюда, но из консоли не позволяет менять проперти файл, че за нах?
+        System.getProperty("url","mango");
 
         Configuration.browser = Project.config.browser();
         Configuration.browserVersion = Project.config.browserVersion();
