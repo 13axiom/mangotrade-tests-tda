f0232e9aa4bf31f4c59c88fee29bededaa755a65
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-04-19 00:36:15 +0300

    add failed login api test

--- src/test/java/com/mangotrade/tests/LoginPage.java
+++ src/test/java/com/mangotrade/tests/LoginPage.java
@@ -16,6 +16,7 @@ import static com.mangotrade.helpers.AllureRestAssuredFilter.withCustomTemplates
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -60,8 +61,6 @@ public class LoginPage extends TestBase {
             $("[data-test-id=login-submit-button]").click();
             sleep(10000);
         });
-
-        sleep(10000);//todo delete?
 
         step("Redirect to 'https://trade.mangotrade.com/traderoom'", () -> {
             String currURL = DriverUtils.getCurrUrl();
@@ -190,8 +189,6 @@ public class LoginPage extends TestBase {
                 assertEquals("https://trade.mangotrade.com/en/register", currURL);
             });
 
-            sleep(10000);//todo delete?
-
             step("Check form header -> should be 'Sing Up'", () -> {
                 $(".RegisterFormContainer").$("h1").shouldHave(exactText("Sign Up"));
             });
@@ -278,6 +275,38 @@ public class LoginPage extends TestBase {
 --header 'Cookie: device_id=3YGp1PaZcCkiO7zazNa2; web_rules=; init_url=https://trade.mangotrade.com/en/login; platform=90; landing=trade.mangotrade.com; lang=en_US; pll_language=en; _gcl_au=1.1.1465944008.1650311546; _ga_BH1SENMS6L=GS1.1.1650311546.1.1.1650311693.0; _ga=GA1.2.2130918845.1650311546; _ym_uid=1650311547675546183; _ym_d=1650311547; _ym_visorc=b; _ym_isad=2; _fbp=fb.1.1650311547145.1764646748; afUserId=c7bb0519-2087-48b0-b307-4b61610965cb-p; AF_SYNC=1650311547841; _gid=GA1.2.1131175257.1650311548; platform_version=2446.5.0946.release; referrer=https://trade.mangotrade.com/traderoom; ssid=054fa7be0f836899ee76da4c7aca8bb8' \
 --data-raw '{"identifier":"mnenie@bk.ru","password":"Test1234"}'
         */
+    }
+
+    @Test
+    @Tag("api_test")
+    @Owner("DmitriyTQC")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Login page api-functional tests")
+    @Description("Rest API test 3")
+    @DisplayName("Failed login without password (API test)")
+    void failedLoginNullPasswordViaRestAPI() {
+        open();//todo костыль, потому что в beforeall вызывается вебдрайвер и он ожидает урл
+        LoginData data = new LoginData();
+        data.setIdentifier("mnenie@bk.ru");
+
+        given()
+                .filter(withCustomTemplates())
+                .contentType("application/json")
+                .cookie("platform=90; " +
+                        "lang=en_US; " +
+                        "ssid=054fa7be0f836899ee76da4c7aca8bb8")
+                .header("Connection", "keep-alive")
+                .body(data)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/api/v2/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(401)
+                .body("code", is("invalid_credentials"))
+                .body("message", containsString("entered the wrong credentials"));
     }
 
 }
