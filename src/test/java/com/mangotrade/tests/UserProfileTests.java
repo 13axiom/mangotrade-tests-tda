55654a86dd5a16b188f8d9c55d0cd392812f0857
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-05-26 18:23:21 +0300

    ui+api intermediate commit v2

--- src/test/java/com/mangotrade/tests/UserProfileTests.java
+++ src/test/java/com/mangotrade/tests/UserProfileTests.java
@@ -4,16 +4,13 @@ import com.mangotrade.actions.AuthActions;
 import com.mangotrade.actions.UserProfileActions;
 import com.mangotrade.config.Project;
 import io.qameta.allure.*;
-import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.response.Response;
-import models.LoginData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
-import static com.mangotrade.tests.Specs.*;
-import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 
 public class UserProfileTests {
@@ -28,14 +25,13 @@ public class UserProfileTests {
     void checkProfile() {
         AuthActions authActions = new AuthActions();
         UserProfileActions userProfileActions = new UserProfileActions();
-        LoginData data = new LoginData();
 
         String ssid = authActions.getAuthorizationCookie();
         Response userProfile = userProfileActions.getUserProfile(ssid);
 
         userProfileActions.checkUserFirstName(userProfile, Project.userData.firstName());
-        userProfileActions.checkUserLastName(userProfile,Project.userData.lastName());
-        userProfileActions.checkUserEmail(userProfile,Project.userData.userLogin());
+        userProfileActions.checkUserLastName(userProfile, Project.userData.lastName());
+        userProfileActions.checkUserEmail(userProfile, Project.userData.userLogin());
         userProfileActions.checkUserCountry(userProfile, Project.userData.userCountry());
 
         String userId = userProfileActions.getUserId(userProfile);
@@ -43,5 +39,22 @@ public class UserProfileTests {
 
     }
 
+    @Test
+    @Tags({@Tag("positive"), @Tag("user_profile"), @Tag("rest_api"), @Tag("profile")})
+    @Owner("DmitriyTQC")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("User Profile -> REST_API")
+    @DisplayName("Validate JSON Schema of User Profile")
+    @Description("Rest API test #7")
+    void validateJSONSchemaUserProfile() {
+        AuthActions authActions = new AuthActions();
+        UserProfileActions userProfileActions = new UserProfileActions();
+
+        String ssid = authActions.getAuthorizationCookie();
+        Response userProfile = userProfileActions.getUserProfile(ssid);
+
+        userProfile.then()
+                .assertThat().body(matchesJsonSchemaInClasspath("schemas/userProfile.json"));
+    }
 
 }
